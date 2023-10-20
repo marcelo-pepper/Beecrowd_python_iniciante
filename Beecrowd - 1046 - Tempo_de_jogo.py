1a782e0906763ed6a9e28d212d8ac7ba193adedb
Marcelo Pimenta <101263910+marcelo-pepper@users.noreply.github.com>
2023-10-20 10:29:52 -0300

    Update Beecrowd - 1046 - Tempo_de_jogo.py

--- Beecrowd - 1046 - Tempo_de_jogo.py
+++ Beecrowd - 1046 - Tempo_de_jogo.py
@@ -6,7 +6,6 @@ if hora_final < hora_inicial:
     duracao = (24 - hora_inicial) + hora_final
 else:
     duracao = hora_final - hora_inicial
-
 if duracao < 1:
     duracao += 24
 print('O JOGO DUROU {} HORA(S)'.format(duracao))
