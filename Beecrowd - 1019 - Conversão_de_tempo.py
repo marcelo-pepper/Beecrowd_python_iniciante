5d2f11eac6bdc83a5c511be8e29c139da20d7bbf
Marcelo Pimenta <101263910+marcelo-pepper@users.noreply.github.com>
2023-10-20 10:27:06 -0300

    Update Beecrowd - 1019 - Conversão_de_tempo.py

--- Beecrowd - 1019 - Conversão_de_tempo.py
+++ Beecrowd - 1019 - Conversão_de_tempo.py
@@ -1,12 +1,8 @@
 tempo = int(input())
-
 tempo_hora = tempo // 3600
 tempo = tempo - tempo_hora * 3600
-
 tempo_minuto = tempo // 60
 tempo = tempo - tempo_minuto * 60
-
 tempo_segundo = tempo // 1
 tempo = tempo - tempo_segundo * 1
-
 print('{}:{}:{}'.format(tempo_hora, tempo_minuto, tempo_segundo))
