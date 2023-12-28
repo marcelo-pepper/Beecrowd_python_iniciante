0acde728184311d1ff36371abd41fcf39ba9d9ed
Marcelo Pimenta <101263910+marcelo-pepper@users.noreply.github.com>
2023-12-28 15:28:03 -0300

    Add files via upload

---
+++ Beecrowd - 1052 - Mês.py
@@ -0,0 +1,25 @@
+numero = int(input())
+if numero == 1:
+    print('January')
+if numero == 2:
+    print('February')
+if numero == 3:
+    print('March')
+if numero == 4:
+    print('April')
+if numero == 5:
+    print('May')
+if numero == 6:
+    print('June')
+if numero == 7:
+    print('July')
+if numero == 8:
+    print('August')
+if numero == 9:
+    print('September')
+if numero == 10:
+    print('October')
+if numero == 11:
+    print('November')
+if numero == 12:
+    print('December')
